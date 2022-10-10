4e6f45e7e68f390d52284aa927bb4365cc7b4708
Niihara Megumu <nhrmgm1223@gmail.com>
2022-10-10 15:20:00 +0900

    add not null

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_10_015801) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_10_061034) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
